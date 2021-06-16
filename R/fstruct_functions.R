25b1948b62550df4d0dec28d0053c05d7c5888c9
Maike Morrison <maike.morrison@gmail.com>
2021-06-15 22:15:48 -0700

    Update documentation and examples

--- R/fstruct_functions.R
+++ R/fstruct_functions.R
@@ -1,6 +1,6 @@
 # THIS FILE CONTAINS:
 # Q_plot - generate a population structure plot using ggplot2
-# Q_stat - generate fst, fstmax, and fst/fstmax for a Q matrix
+# Q_stat - generate fst, fstmax, and Fst/FstMax for a Q matrix
 # Q_bootstrap - generate bootstrap Q matrices and related plots and statistics
 # Q_simulate - simulate Q matrices using the Dirichlet distribution
 
@@ -108,7 +108,6 @@ Q_plot <- function(Q, K){
 #' K = 3
 #' )
 #'
-#'@importFrom dplyr %>%
 #' @export
 Q_stat <- function(Q, K){
   # Check if Q matrix is in STRUCTURE/ADMIXTURE output form, or if it only contains columns of ancestry coefficients
@@ -149,6 +148,45 @@ Q_stat <- function(Q, K){
 }
 
 
+# Q_bootstrap ----------------------------------------
+#' Generate and analyze bootstrap replicates of one or more Q matrices
+#'
+#' Generates bootstrap replicate Q matrices, computes Fst/FstMax for each bootstrap replicate, produces several plots of the bootstrap distributions of Fst/FstMax for each provided Q matrix, and runs two statistical tests comparing these bootstrap distributions. The tests comparing bootstrap distributions of Fst/FstMax facilitate statistical comparison of the variability in each of multiple Q matrices.
+#'
+#' @param matrices A dataframe, matrix, or array representing a Q matrix, or a (possibly named) list of arbitrarily many such objects. For each Q matrix, matrix rows represents an individual and the last \code{K} columns contain individual membership coefficients (when restricted to the last \code{K} columns, the rows should sum to approximately 1). If the list of matrices is not named, the matrices are numbered and the numbers are used in place of names.
+#' @param n_replicates The number of bootstrap replicate matrices to generate for each provided Q matrix.
+#' @param K The number of ancestral clusters in the Q matrix. Each individual should have \code{K} membership coefficients.
+#'
+#' @return A named list of containing the following entries:
+#' \itemize{
+#' \item \code{bootstrap_replicates}: A named list of lists. Each element is named for a Q matrix provided in \code{matrices} and contains a list of \code{n_replicates} bootstrap replicates of the provided matrix. E.g., if \code{n_replicates = 100} and the first Q matrix in \code{matrices} is named \code{A}, then the first element of \code{bootstrap_replicates}, \code{bootstrap_replicates$bootstrap_matrices_A}, is itself a list of 100 matrices, each representing a bootstrap replicate of matrix A.
+#' \item \code{statistics}: A dataframe containing the output of \code{Q_stat}: \code{Fst}, \code{FstMax}, and \code{ratio} (Fst/FstMax), computed for each bootstrap replicate matrix in \code{bootstrap_replicates}. The ratio Fst/FstMax quantifies the variability of each  Q matrix. The first column, titled \code{Matrix}, is a factor indicating which provided Q matrix the row corresponds to (the matrix name if \code{matrices} is a named list, or a number otherwise). The row names are of the form \code{stats_matrix.replicate} where \code{matrix} is the name of one of the provided Q matrices (or the entry number if the list elements were not named) and replicate is the number of bootstrap replicate (rep takes values from 1 to \code{n_replicates}).
+#' \item \code{plot_boxplot}: A ggplot2 box plot depicting the bootstrap distribution of Fst/FstMax for each matrix in \code{matrices}.
+#' \item \code{plot_violin}: A ggplot2 violin plot depicting the bootstrap distribution of Fst/FstMax for each matrix in \code{matrices}.
+#' \item \code{plot_ecdf}: A ggplot2 empirical cumulative distribution function plot depicting the bootstrap distribution of Fst/FstMax for each matrix in \code{matrices}.
+#' \item \code{test_kruskal_wallis}: Results of a Kruskal-Wallis test performed on the bootstrap distributions of Fst/FstMax. This test is a non-parametric statistical test of whether all provided bootstrap distributions are identically distributed.
+#' \item \code{test_pairwise_wilcox}: Results of a Wilcoxon rank-sum test performed on the bootstrap distributions of Fst/FstMax. This test is a non-parameteric statistical test of whether \emph{each pairwise combination} of provided bootstrap distributions is identically distributed.
+#' }
+#' @examples
+#' A = matrix(c(.4,.2,.4,
+#'              .5,.4,.1,
+#'              .6,.1,.3,
+#'              .6,.3,.1),
+#'            nrow = 4,
+#'            byrow = TRUE)
+#'
+#' B = matrix(c(0,0,1,
+#'              .5,.5,0,
+#'              .5,.4,.1,
+#'              .6,.2,.2,
+#'              .6,.4,0),
+#'            nrow = 5,
+#'            byrow = TRUE)
+#'
+#' bs <- Q_bootstrap(matrices = list(A=A, B=B),
+#'                   n_replicates = 10, K = 3)
+#'
+#'@importFrom dplyr %>%
 #' @export
 Q_bootstrap <- function(matrices, n_replicates, K){
   ## INPUT:
@@ -158,7 +196,7 @@ Q_bootstrap <- function(matrices, n_replicates, K){
   ## OUTPUT:
   # bootstrap_replicates = list of matrices of bootstrap replicates for each input matrix
   # statistics = dataframe; rows = bootstrap reps, columns = Matrix, Statistic, Value
-  # histogram, CDF, boxplot of Fst/Fst max ratio bootstrap estimates
+  # histogram, CDF, boxplot of Fst/FstMax ratio bootstrap estimates
 
   # Do computations if matrices = a single matrix ---------------------------------------
 
@@ -180,7 +218,7 @@ Q_bootstrap <- function(matrices, n_replicates, K){
     matrix = matrices
     # Generate bootstrap data sets
     for(replicate in 1:n_replicates){
-      bootstrap_matrices_Q[[replicate]] <- matrix[rdunif(n = nrow(matrix),
+      bootstrap_matrices_Q[[replicate]] <- matrix[purrr::rdunif(n = nrow(matrix),
                                                          a = 1, b = nrow(matrix)),]
     }
 
@@ -190,7 +228,6 @@ Q_bootstrap <- function(matrices, n_replicates, K){
       unlist %>%
       matrix(ncol = 3, byrow = TRUE) %>% data.frame() %>%
       `colnames<-`(c("Fst", "FstMax", "ratio"))
-
 
 
     # Do computations if matrices = a list ---------------------------------------------
@@ -220,7 +257,7 @@ Q_bootstrap <- function(matrices, n_replicates, K){
 
       # Generate bootstrap data sets
       for(replicate in 1:n_replicates){
-        bs_list[[replicate]] <- matrix[rdunif(n = nrow(matrix),
+        bs_list[[replicate]] <- matrix[purrr::rdunif(n = nrow(matrix),
                                               a = 1, b = nrow(matrix)),]
       }
 
@@ -234,13 +271,13 @@ Q_bootstrap <- function(matrices, n_replicates, K){
       # Find matrices for which this is the case, and replace them and their statistics
 
       while(sum(stats$ratio < 0)){
-        # Which bootstrap matrices have negative values for fst/fstmax?
+        # Which bootstrap matrices have negative values for Fst/FstMax?
         negatives <- which(stats$ratio < 0)
 
         # Replace those bootstrap replicates with new, random bootstrap replicates
         bs_list[negatives] <- lapply(X = 1:length(negatives),
                                      FUN = function(x){
-                                       matrix[rdunif(n = nrow(matrix),
+                                       matrix[purrr::rdunif(n = nrow(matrix),
                                                      a = 1, b = nrow(matrix)),]
                                      })
 
@@ -274,40 +311,41 @@ Q_bootstrap <- function(matrices, n_replicates, K){
 
   all_stats$Matrix <- factor(all_stats$Matrix, levels = unique(all_stats$Matrix))
 
-  plot_ecdf <- ggplot(data = all_stats) +
-    stat_ecdf(aes(x = ratio, color = Matrix)) +
-    xlab(TeX('Fst/FstMax')) + ylab("Cumulative Probability") +
-    xlim(0,1) + theme_bw() + scale_color_viridis_d()
+  plot_ecdf <- ggplot2::ggplot(data = all_stats) +
+    ggplot2::stat_ecdf(ggplot2::aes(x = ratio, color = Matrix)) +
+    ggplot2::xlab(TeX('Fst/FstMax')) +
+    ggplot2::ylab("Cumulative Probability") +
+    ggplot2::xlim(0,1) + ggplot2::theme_bw() + ggplot2::scale_color_viridis_d()
 
-  plot_boxplot <- ggplot(data = all_stats,
-                         aes(x = Matrix, y = ratio)) +
-    geom_boxplot() +
-    ylab(TeX('Fst/FstMax')) + xlab("") +
-    theme_bw()
+  plot_boxplot <- ggplot2::ggplot(data = all_stats,
+                                  ggplot2::aes(x = Matrix, y = ratio)) +
+    ggplot2::geom_boxplot() +
+    ggplot2::ylab(TeX('Fst/FstMax')) + ggplot2::xlab("") +
+    ggplot2::theme_bw()
 
-  plot_violin <- ggplot(data = all_stats,
-                        aes(x = Matrix, y = round(ratio,5))) +
-    geom_violin(scale = "width") + geom_boxplot(width = 0.3) +
-    ylab(TeX('Fst/FstMax')) + xlab("") +
-    theme_bw()
+  plot_violin <- ggplot2::ggplot(data = all_stats,
+                                 ggplot2::aes(x = Matrix, y = round(ratio,5))) +
+    ggplot2::geom_violin(scale = "width") + ggplot2::geom_boxplot(width = 0.3) +
+    ggplot2::ylab(TeX('Fst/FstMax')) + ggplot2::xlab("") +
+    ggplot2::theme_bw()
 
-  test_kruskal_wallace <- kruskal.test(ratio ~ Matrix,
+  test_kruskal_wallis <- stats::kruskal.test(ratio ~ Matrix,
                                        data= all_stats)
 
-  test_pairwise_wilcox <-  pairwise.wilcox.test(x = all_stats$ratio,
+  test_pairwise_wilcox <-  stats::pairwise.wilcox.test(x = all_stats$ratio,
                                                 g = all_stats$Matrix,
                                                 paired = FALSE)
 
   bootstrap_replicates <- mget(paste0("bootstrap_matrices_",
                                       names))
 
-    return(list(statistics = all_stats,
-              bootstrap_replicates = bootstrap_replicates,
-              plot_boxplot = plot_boxplot,
-              plot_violin = plot_violin,
-              plot_ecdf = plot_ecdf,
-              test_kruskal_wallace = test_kruskal_wallace,
-              test_pairwise_wilcox = test_pairwise_wilcox))
+    return(list(bootstrap_replicates = bootstrap_replicates,
+                statistics = all_stats,
+                plot_boxplot = plot_boxplot,
+                plot_violin = plot_violin,
+                plot_ecdf = plot_ecdf,
+                test_kruskal_wallis = test_kruskal_wallis,
+                test_pairwise_wilcox = test_pairwise_wilcox))
 }
 
 
@@ -316,18 +354,53 @@ Q_bootstrap <- function(matrices, n_replicates, K){
 
 
 
-# Q simulate ---------------------------------------------------------------------------------
-Q_simulate <- function(alpha, q, rep, popsize, seed){
+
+
+
+# Q_simulate ----------------------------------------
+#' Simulate one or more Q matrices using the Dirichlet distribution
+#'
+#' Simulates Q matrices by drawing vectors of membership coefficients from a Dirichlet distrubtion parameterized by two variables: \eqn{\alpha}, which controls variability, and \eqn{\lambda=(\lambda_1, \lambda_2, ...., \lambda_K)} which controls the mean of each of the K ancestry coefficients.
+#'
+#' @param alpha A number that sets the variability of the membership coefficients. The variance of coefficient k is Var[x_k] = \eqn{\lambda_k/(\alpha+1)}. Larger values of \eqn{\alpha} lead to lower variability.
+#' @param lambda A vector that sets the mean membership of each ancestral cluster across the population. The vector should sum to 1.
+#' @param rep The number of Q matrices to generate.
+#' @param popsize The number of individuals to include in each Q matrix.
+#' @param seed Optional; sets the random seed. Use if reproducibility of random results is desired.
+#'
+#' @return A data frame containing the simulated Q matrices. Each row represents a single simulated individual. The data frame has the following columns
+#' \itemize{
+#' \item \code{rep}: Which random Q matrix the row belongs to (a number between 1 and the parameter \code{rep})
+#' \item \code{ind}: Which individual in each Q matrix the row corresponds to (a number between 1 and the parameter \code{popsize})
+#' \item \code{alpha}: The alpha value used to simulate the Q matrix.
+#' \item \code{Pop}: alpha_rep (where alpha and rep are the columns described above). Serves as a unique identifier for each Q matrix (useful if running simulations with many different values of \eqn{\alpha}).
+#' \item \code{lambda1, lambda2, etc.}: Membership coefficients (sum to 1).
+#' }
+#'
+#' @examples
+#' # Simulate 100 random Q matrices.
+#' # Each Q matrix has 100 individuals.
+#' # On average these individuals have mean ancestry (1/2, 1/4, 1/4)
+#' # from each of 3 ancestral clusters.
+#' # The variance of each cluster x_i is Var[x_i] = lambda_i/(alpha + 1)
+#' # Here lambda_1 = 1/2, lambda_2 = lambda_3 = 1/4
+#'
+#' Q_list <- Q_simulate(alpha = 1, lambda = c(1/2, 1/4, 1/4),
+#'                     rep = 100, popsize = 50, seed = 1)
+#'
+#'@importFrom dplyr %>%
+#' @export
+Q_simulate <- function(alpha, lambda, rep, popsize, seed){
 
   # How many clusters are there?
-  K = length(q)
+  K = length(lambda)
 
   if(!missing(seed)){set.seed(seed)}
 
-  # (a1, a2, ...) = (q1, q2, ...)*alpha parameterizes the Dirichlet distribution
+  # (a1, a2, ...) = (lambda1, lambda2, ...)*alpha parameterizes the Dirichlet distribution
   alpha_vec = alpha %>%
     data.frame("alpha" = .) %>%
-    cbind(apply(X = ., MARGIN = 1, FUN = function(a) a*q) %>%
+    cbind(apply(X = ., MARGIN = 1, FUN = function(a) a*lambda) %>%
             matrix(byrow=TRUE, ncol=K)) %>% # this step necessary for K=1 case
     `colnames<-`(c("alpha", sapply(1:K, function(k) paste0("a",k))))
 
@@ -342,17 +415,17 @@ Q_simulate <- function(alpha, q, rep, popsize, seed){
                           alpha = a) %>%
                  round(10)
              }) %>%
-    data.frame %>% ### Restructure output so the columns are: individual, alphas, q1, q2, rep
+    data.frame %>% ### Restructure output so the columns are: individual, alphas, lambda1, lambda2, rep
     `colnames<-`(alpha_vec$alpha) %>%
-    mutate(ind = rep(1:popsize,K),
-           q = (sapply(X = 1:K,
-                       FUN = function(x) paste0("q", x)) %>%
-                  lapply(function(q) rep(q, popsize)) %>%
+    dplyr::mutate(ind = rep(1:popsize,K),
+           lambda = (sapply(X = 1:K,
+                       FUN = function(x) paste0("lambda", x)) %>%
+                  lapply(function(lambda) rep(lambda, popsize)) %>%
                   unlist)) %>%
     tidyr::pivot_longer(cols = 1:length(alpha),
                  names_to = "alpha",
                  values_to = "Q") %>%
-    tidyr::pivot_wider(names_from = q,
+    tidyr::pivot_wider(names_from = lambda,
                 values_from = Q) %>%
     cbind("rep" = 1, .)
 
@@ -369,17 +442,17 @@ Q_simulate <- function(alpha, q, rep, popsize, seed){
                              alpha = a) %>%
                     round(10)
                 }) %>%
-            data.frame %>% ### Restructure output so the columns are: individual, alphas, q1, q2, rep
+            data.frame %>% ### Restructure output so the columns are: individual, alphas, lambda1, lambda2, rep
             `colnames<-`(alpha_vec$alpha) %>%
-            mutate(ind = rep(1:popsize,K),
-                   q = (sapply(X = 1:K,
-                               FUN = function(x) paste0("q", x)) %>%
-                          lapply(function(q) rep(q, popsize)) %>%
+            dplyr::mutate(ind = rep(1:popsize,K),
+                   lambda = (sapply(X = 1:K,
+                               FUN = function(x) paste0("lambda", x)) %>%
+                          lapply(function(lambda) rep(lambda, popsize)) %>%
                           unlist)) %>%
             tidyr::pivot_longer(cols = 1:length(alpha),
                          names_to = "alpha",
                          values_to = "Q") %>%
-            tidyr::pivot_wider(names_from = q,
+            tidyr::pivot_wider(names_from = lambda,
                         values_from = Q) %>%
             cbind("rep" = iter, .)
         )
@@ -387,12 +460,12 @@ Q_simulate <- function(alpha, q, rep, popsize, seed){
   }
 
   # Give each population a unique identifier: alpha_rep
-  Q <- Q %>% mutate(ind = as.factor(ind),
+  Q <- Q %>% dplyr::mutate(ind = as.factor(ind),
                     alpha = as.numeric(alpha),
                     Pop = paste(round(alpha,3), rep, sep = "_") %>% as.factor,
                     rep = as.factor(rep),
-                    .before = q1) %>%
-    arrange(rep, Pop, ind)
+                    .before = lambda1) %>%
+    dplyr::arrange(rep, Pop, ind)
   return(Q)
 }
 
